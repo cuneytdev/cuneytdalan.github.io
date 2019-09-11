cf77b70c4bb06ace2367928589c46e66d5e0cfe2
Cuneyt Dalan <cuneytdalan17@gmail.com>
2019-09-11 14:16:35 +0300

    first
    last commit

---
+++ dist/js/main.js
@@ -0,0 +1,38 @@
+//SELECT DOM ELEMENTS
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+const navItems = document.querySelectorAll('.nav-item');
+
+//SET INITTIAL STATE OF MENU
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+
+        navItems.forEach(item => item.classList.add('show'));
+
+        //SET MENU STATE
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+
+        navItems.forEach(item => item.classList.remove('show'));
+
+        //SET MENU STATE
+        showMenu = false;
+    }
+}
